9664a744a99ad08d8cf256d351dd46b8abb26a44
deniscoman <denis.coman13@gmail.com>
2022-03-09 15:51:18 +0200

    Solve Challenge 1

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -11,21 +11,54 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  receive() external payable {
+    this.stake{value: msg.value}();
+  }
 
+  mapping (address => uint256) public balances;
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  uint public constant threshold = 1 ether;
 
+  bool public openForWithdraw = false;
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  uint256 public deadline = block.timestamp + 30 seconds;
 
+  function stake() payable external onlyBeforeDeadline() {
+    balances[msg.sender] += msg.value;
+  }
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function execute() external onlyAfterDeadline notCompleted {
+    if(address(this).balance > threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else {
+      openForWithdraw = true;
+    }
+  }
 
+  function withdraw(address withdrawAddress) external {
+    require(openForWithdraw == true);
+    payable(withdrawAddress).transfer(address(this).balance);
+  }
 
-  // Add the `receive()` special function that receives eth and calls stake()
+  function timeLeft() external view returns (uint256) {
+    if(deadline <= block.timestamp) {
+      return 0;
+    } else return deadline - block.timestamp;
+  }
 
+  modifier onlyAfterDeadline() {
+    require(block.timestamp >= deadline, "Wait until deadline");
+    _;
+  }
 
+  modifier onlyBeforeDeadline() {
+    require(block.timestamp < deadline, "Time is up");
+    _;
+  }
+
+  modifier notCompleted() {
+    bool complete = exampleExternalContract.completed();
+    require(complete == false);
+    _;
+  }
 }
