6cf44c967ec67b8992b98036ac2b4b11b3c84f73
deniscoman <denis.coman13@gmail.com>
2022-03-09 20:35:14 +0200

    Finish Checkpoint 2

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
-contract YourToken {
-    //ToDo: add constructor and mint tokens for deployer,
-    //you can use the above import for ERC20.sol. Read the docs ^^^
+contract YourToken is ERC20{
+  constructor(uint256 initialSupply) public ERC20("AgileFreaks", "AF") {
+       _mint(address(0x1A72b429372352B92201bB7Cd42dF4baD68e037b), initialSupply);
+   }
 }
